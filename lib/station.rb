86dbd60bf14a03ae3a38b2585755516e895ed42e
Anna Valkov & Andrew M Benavides <student@epicodus.com>
2014-08-08 11:22:13 -0700

    save a station to the DB

--- lib/station.rb
+++ lib/station.rb
@@ -4,7 +4,24 @@ class Station
 
   def initialize(station_info)
     @name = station_info["name"]
-    @id = station_info["id"]
+    @id = station_info["id"].to_i
   end
+
+  def self.all
+    stations = []
+    results = DB.exec("SELECT * FROM station;")
+    results.each do |result|
+      new_station = Station.new(result)
+      stations << new_station
+    end
+    stations
+  end
+
+  def save
+    result = DB.exec("INSERT INTO station (name) VALUES ('#{name}') RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+
+
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,8 +1,26 @@
 require 'spec_helper'
 
 describe 'station' do
-  it 'initializes with a name' do
-    new_station = Station.new(:name => "Pteradactyl")
-    expect(new_station).to be_an_instance_of Station
+  describe 'initialize' do
+    it 'initializes with a name' do
+      new_station = Station.new({"name" => "Pteradactyl"})
+      expect(new_station).to be_an_instance_of Station
+    end
   end
+
+  describe 'self.all' do
+    it 'pulls stations out of the DB and saves them as objects' do
+      expect(Station.all).to eq []
+    end
+  end
+
+  describe "save" do
+    it "saves a station to the database" do
+      new_station = Station.new({"name" => "Pteradactyl"})
+      new_station.save
+      p new_station
+      expect(Station.all.length).to eq 1
+    end
+  end
+
 end
